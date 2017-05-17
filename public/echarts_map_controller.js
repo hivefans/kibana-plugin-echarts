d0de9468fc0accbaac38a5f8fa8b2d3cd538ce43
hivefans <shidongjie@gmail.com>
2017-05-17 14:46:30 +0800

    update convertProvince function

--- public/echarts_map_controller.js
+++ public/echarts_map_controller.js
@@ -21,22 +21,22 @@ module.controller('EchartsMapController', function ($scope, $element, $rootScope
 
   var option = {
         title: {
-            text: 'CDN访问统计地图',
+            text: '',
             x: 'center',
             textStyle: {
                 fontSize: 14,
-                olor:"#a6a6a6"
+                color:"#a6a6a6"
             }
         },
         visualMap: {
             min: 0,
             max: 1024,
             inRange: {
-                color: ['#0ba800','#eac736','#d94e5d'].reverse()
+                color: ['#0ba800','#eac736','#d94e5d']
             },
             left: 'left',
             top: 'bottom',
-            text: ['最大值','最小值'], 
+            text: ['高','低'], 
             calculable: true,
             textStyle:{
                 color:"#a6a6a6"
@@ -57,7 +57,7 @@ module.controller('EchartsMapController', function ($scope, $element, $rootScope
             trigger: 'item'
         },
         series: [{
-            name: 'CDN日志',
+            name: '数据统计',
             type: 'map',
             mapType: 'china',
             // coordinateSystem: 'geo',
@@ -74,37 +74,81 @@ module.controller('EchartsMapController', function ($scope, $element, $rootScope
         }]
     };
 
-
     //state.query
+    var provinces = [
+        { name: "anhui", value: "安徽" },
+        { name: "beijing", value: "北京" },
+        { name: "fujian", value: "福建" },
+        { name: "gansu", value: "甘肃" },
+        { name: "guangdong", value: "广东" },
+        { name: "guangxi", value: "广西" },
+        { name: "guizhou", value: "贵州" },
+        { name: "hainan", value: "海南" },
+        { name: "hebei", value: "河北" },
+        { name: "henan", value: "河南" },
+        { name: "hubei", value: "湖北" },
+        { name: "hunan", value: "湖南" },
+        { name: "jilin", value: "吉林" },
+        { name: "jiangsu", value: "江苏" },
+        { name: "jiangxi", value: "江西" },
+        { name: "liaoning", value: "辽宁" },
+        { name: "ningxia", value: "宁夏" },
+        { name: "qinghai", value: "青海" },
+        { name: "shandong", value: "山东" },
+        { name: "sanxi", value: "山西" },
+        { name: "shanxi", value: "陕西" },
+        { name: "shanghai", value: "上海" },
+        { name: "sichuan", value: "四川" },
+        { name: "tianjin", value: "天津" },
+        { name: "xizang", value: "西藏" },
+        { name: "xinjiang", value: "新疆" },
+        { name: "yunnan", value: "云南" },
+        { name: "zhejiang", value: "浙江" },
+        { name: "chongqing", value: "重庆" },
+        { name: "aomen", value: "澳门" },
+        { name: "xianggang", value: "香港" },
+        { name: "taiwan", value: "台湾" },
+        { name: "heilongjiang", value: "黑龙江" },
+        { name: "neimenggu", value: "内蒙古" }
+    ]
+
+    // 转化省份到汉字
+    var convertProvince = function (data) {
+        for (var i = 0; i < provinces.length; i++) {
+            if (provinces[i].name == data) {
+                return provinces[i].value;
+            }
+        }
+        // console.log(data)
+        return "other";
+    }
+
     
-    var tableGroups;
-    
-    
+    var tableGroups;   
     $scope.$watch('esResponse', function(resp) {
       if (!resp) {
         return;
       }
         var avgArr = new Array();
         option.series[0].data.length=0;
-        //  console.log("=========resp=========");
-        //  console.log(resp);
         tableGroups = tabifyAggResponse($scope.vis, resp);
-        // console.log(tableGroups);
-        // console.log("=====option=====")
-        //  console.log(option);
         tableGroups.tables.forEach(function (table,index) {
             var cols = table.columns;
             table.rows.forEach(function (row,i) {
                 var region_name = row[0].toString();
                 var avg_speed = row[1];
+                avg_speed=avg_speed.toFixed(2);
                 avgArr.push(avg_speed);
+                if(escape(region_name).indexOf("%u")<0) {
+                    region_name = convertProvince(region_name);
+                  }
+                console.log("======"+region_name);
                 option.series[0].data.push(
                     {
                         name:region_name,
-                        value:avg_speed.toFixed(2)
+                        value:avg_speed
                     }
                 )
-
             });
         });
         mychart.clear();
@@ -133,3 +177,4 @@ module.controller('EchartsMapController', function ($scope, $element, $rootScope
 		true
 	);
   });
+
